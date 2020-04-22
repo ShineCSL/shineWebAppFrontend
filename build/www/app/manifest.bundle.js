92077dd21f872a4677398ff785af992074c12495
rjutliah <rjutliah@shinecsl.com>
2020-04-22 14:52:57 +0400

    ressource

--- build/www/app/manifest.bundle.js
+++ build/www/app/manifest.bundle.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "9ff73dccc0bef7124409"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "cdbbc96f2fd0abd5f409"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
