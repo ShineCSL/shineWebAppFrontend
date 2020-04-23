c79377e81909b8b56fd03c40182c2d23c75d413f
rjutliah <rjutliah@shinecsl.com>
2020-04-23 13:55:01 +0400

    css hovers

--- build/www/app/manifest.bundle.js
+++ build/www/app/manifest.bundle.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "3751effc5c514d99dfe8"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "3cf0eadab38919005d80"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
